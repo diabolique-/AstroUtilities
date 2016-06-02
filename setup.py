f2a5e20df40c04b141b7f9089a0bc57a50a7af5a
gillenbrown <gillenbrown@gmail.com>
2016-06-02 15:33:04 -0500

    changed to MIT license

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 	name="sinistra",
 
-	version="0.2.1",
+	version="0.2.2",
 
 	description="A collection of astronomy related tools.",
 	long_description="A collection of astronomy related tools. This project contains a bunch of functions and classes I've written in my time doing astronomy. They may or may not be useful to anyone else.\nThe name sinistra comes from the name of the Hogwarts professor of astronomy in the Harry Potter books, who is names Professor Sinistra.",
@@ -13,7 +13,7 @@ setup(
 	author="Gillen Brown",
 	author_email="gillenbrown@gmail.com",
 
-	license="GNU GPLv3",
+	license="MIT",
 
 	keywords="astronomy tools utilities astrophysics",
 
